fe2759f45d11d931becb39ea492be469af3efcfc
GooseAkukaracha <103554799+GooseAkukaracha@users.noreply.github.com>
2023-03-01 18:11:17 +0300

    new

--- Base.py
+++ Base.py
@@ -13,3 +13,17 @@ class Base:
         self.y = y
         self.x1 = 0
         self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
